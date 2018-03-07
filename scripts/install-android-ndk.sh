02f681b1a82560ec2031b0f9c23aa2cd9e0a7546
Aaron Alaniz <aalaniz@twilio.com>
2018-03-07 10:32:44 -0600

    Check if NDK exists before downloading

--- scripts/install-android-ndk.sh
+++ scripts/install-android-ndk.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-pushd /opt/android-ndk
-wget -q https://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip
-unzip -q android-ndk-r12b-linux-x86_64.zip
-mv android-ndk-r12b r12b
-rm android-ndk-r12b-linux-x86_64.zip
-popd
+if [ ! -d /opt/android-ndk/r12b ]; then
+    pushd /opt/android-ndk
+    wget -q https://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip
+    unzip -q android-ndk-r12b-linux-x86_64.zip
+    mv android-ndk-r12b r12b
+    rm android-ndk-r12b-linux-x86_64.zip
+    popd
+fi
